eec3db956051e8ce275f14cd54c79cf014eb3a1c
ZyberPE <zyberpe@gmail.com>
2026-03-08 17:44:51 -0400

    Update Main.php fixes

--- src/LDX/iProtector/Main.php
+++ src/LDX/iProtector/Main.php
@@ -12,18 +12,13 @@ use pocketmine\command\CommandSender;
 
 use pocketmine\player\Player;
 
-use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\event\block\BlockPlaceEvent;
-
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\event\player\PlayerBucketEvent;
-
-use pocketmine\event\entity\EntityDamageEvent;
-
 use pocketmine\math\Vector3;
 use pocketmine\world\Position;
 
-use pocketmine\utils\TextFormat;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\player\PlayerBucketEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 
 class Main extends PluginBase implements Listener{
 
@@ -47,15 +42,14 @@ class Main extends PluginBase implements Listener{
 
 		$data = json_decode(file_get_contents($this->getDataFolder()."areas.json"),true);
 
-		foreach($data as $datum){
-
+		foreach($data as $d){
 			new Area(
-				$datum["name"],
-				$datum["flags"],
-				new Vector3(...$datum["pos1"]),
-				new Vector3(...$datum["pos2"]),
-				$datum["level"],
-				$datum["whitelist"],
+				$d["name"],
+				$d["flags"],
+				new Vector3(...$d["pos1"]),
+				new Vector3(...$d["pos2"]),
+				$d["level"],
+				$d["whitelist"],
 				$this
 			);
 		}
@@ -72,79 +66,152 @@ class Main extends PluginBase implements Listener{
 			return false;
 		}
 
-		$player = strtolower($sender->getName());
+		$p = strtolower($sender->getName());
 
 		switch(strtolower($args[0])){
 
 			case "pos1":
-
-				$this->selectingFirst[$player] = true;
-
-				$sender->sendMessage(TextFormat::GREEN."Break or place block for pos1");
+				$this->selectingFirst[$p] = true;
+				$sender->sendMessage("Break/place block to set Pos1.");
 			break;
 
 			case "pos2":
-
-				$this->selectingSecond[$player] = true;
-
-				$sender->sendMessage(TextFormat::GREEN."Break or place block for pos2");
+				$this->selectingSecond[$p] = true;
+				$sender->sendMessage("Break/place block to set Pos2.");
 			break;
 
 			case "create":
 
-				if(!isset($args[1])){
-					$sender->sendMessage("Usage: /area create <name>");
-					return true;
-				}
+				if(!isset($args[1])) return true;
 
-				if(!isset($this->firstPosition[$player],$this->secondPosition[$player])){
-					$sender->sendMessage("Select both positions first.");
+				if(!isset($this->firstPosition[$p],$this->secondPosition[$p])){
+					$sender->sendMessage("Select pos1 and pos2 first.");
 					return true;
 				}
 
 				$name = strtolower($args[1]);
 
-				if(isset($this->areas[$name])){
-					$sender->sendMessage("Area already exists.");
-					return true;
-				}
-
 				new Area(
 					$name,
 					["edit"=>true,"god"=>false,"touch"=>true],
-					$this->firstPosition[$player],
-					$this->secondPosition[$player],
+					$this->firstPosition[$p],
+					$this->secondPosition[$p],
 					$sender->getWorld()->getFolderName(),
-					[$player],
+					[$p],
 					$this
 				);
 
 				$this->saveAreas();
 
-				unset($this->firstPosition[$player],$this->secondPosition[$player]);
+				unset($this->firstPosition[$p],$this->secondPosition[$p]);
 
-				$sender->sendMessage(TextFormat::AQUA."Area created.");
+				$sender->sendMessage("Area created.");
 			break;
 
 			case "list":
 
-				foreach($this->areas as $area){
-					$sender->sendMessage("- ".$area->getName());
+				foreach($this->areas as $a){
+					$sender->sendMessage("- ".$a->getName());
 				}
 
 			break;
 
 			case "delete":
 
-				if(!isset($args[1])){
-					return true;
-				}
+				if(!isset($args[1])) return true;
 
 				$name = strtolower($args[1]);
 
 				if(isset($this->areas[$name])){
 					$this->areas[$name]->delete();
 					$sender->sendMessage("Area deleted.");
+				}
+
+			break;
+
+			case "here":
+
+				foreach($this->areas as $a){
+
+					if($a->contains($sender->getPosition(),$sender->getWorld()->getFolderName())){
+						$sender->sendMessage("Inside area ".$a->getName());
+						return true;
+					}
+				}
+
+				$sender->sendMessage("No area found.");
+			break;
+
+			case "tp":
+
+				if(!isset($args[1])) return true;
+
+				$name = strtolower($args[1]);
+
+				if(!isset($this->areas[$name])){
+					$sender->sendMessage("Area not found.");
+					return true;
+				}
+
+				$a = $this->areas[$name];
+
+				$sender->teleport(new Position(
+					$a->getFirstPosition()->getX(),
+					$a->getFirstPosition()->getY()+1,
+					$a->getFirstPosition()->getZ(),
+					$sender->getWorld()
+				));
+
+			break;
+
+			case "flag":
+
+				if(!isset($args[1],$args[2])) return true;
+
+				$name = strtolower($args[1]);
+
+				if(!isset($this->areas[$name])) return true;
+
+				$area = $this->areas[$name];
+				$flag = strtolower($args[2]);
+
+				if(isset($args[3])){
+					$mode = strtolower($args[3]) === "true";
+				}else{
+					$mode = !$area->getFlag($flag);
+				}
+
+				$area->setFlag($flag,$mode);
+
+				$sender->sendMessage("Flag ".$flag." set to ".($mode?"true":"false"));
+
+			break;
+
+			case "whitelist":
+
+				if(!isset($args[1],$args[2])) return true;
+
+				$name = strtolower($args[1]);
+
+				if(!isset($this->areas[$name])) return true;
+
+				$area = $this->areas[$name];
+
+				switch($args[2]){
+
+					case "add":
+						$area->setWhitelisted(strtolower($args[3]),true);
+					break;
+
+					case "remove":
+						$area->setWhitelisted(strtolower($args[3]),false);
+					break;
+
+					case "list":
+						foreach($area->getWhitelist() as $w){
+							$sender->sendMessage($w);
+						}
+					break;
 				}
 
 			break;
@@ -155,123 +222,49 @@ class Main extends PluginBase implements Listener{
 
 	public function saveAreas() : void{
 
-		$data = [];
+		$data=[];
 
-		foreach($this->areas as $area){
+		foreach($this->areas as $a){
 
-			$data[] = [
-				"name"=>$area->getName(),
-				"flags"=>$area->getFlags(),
-				"pos1"=>[
-					$area->getFirstPosition()->getX(),
-					$area->getFirstPosition()->getY(),
-					$area->getFirstPosition()->getZ()
-				],
-				"pos2"=>[
-					$area->getSecondPosition()->getX(),
-					$area->getSecondPosition()->getY(),
-					$area->getSecondPosition()->getZ()
-				],
-				"level"=>$area->getWorldName(),
-				"whitelist"=>$area->getWhitelist()
+			$data[]=[
+				"name"=>$a->getName(),
+				"flags"=>$a->getFlags(),
+				"pos1"=>[$a->getFirstPosition()->getX(),$a->getFirstPosition()->getY(),$a->getFirstPosition()->getZ()],
+				"pos2"=>[$a->getSecondPosition()->getX(),$a->getSecondPosition()->getY(),$a->getSecondPosition()->getZ()],
+				"level"=>$a->getWorldName(),
+				"whitelist"=>$a->getWhitelist()
 			];
 		}
 
 		file_put_contents($this->getDataFolder()."areas.json",json_encode($data,JSON_PRETTY_PRINT));
 	}
 
-	public function canEdit(Player $player, Position $pos) : bool{
-
-		if($player->hasPermission("iprotector.access")){
-			return true;
-		}
-
-		foreach($this->areas as $area){
-
-			if($area->contains($pos,$pos->getWorld()->getFolderName())){
-
-				if($area->isWhitelisted(strtolower($player->getName()))){
-					return true;
-				}
-
-				if(!$area->getFlag("edit")){
-					return true;
-				}
-
-				return false;
-			}
-		}
-
-		return true;
-	}
-
 	public function onPlace(BlockPlaceEvent $event) : void{
 
-		$player = $event->getPlayer();
-		$name = strtolower($player->getName());
+		$player=$event->getPlayer();
+		$name=strtolower($player->getName());
 
 		foreach($event->getTransaction()->getBlocks() as [$x,$y,$z,$block]){
 
-			$pos = new Vector3($x,$y,$z);
-
 			if(isset($this->selectingFirst[$name])){
 				unset($this->selectingFirst[$name]);
-
-				$this->firstPosition[$name] = $pos;
-
-				$player->sendMessage("Pos1 set");
-
+				$this->firstPosition[$name]=new Vector3($x,$y,$z);
 				$event->cancel();
 				return;
 			}
 
 			if(isset($this->selectingSecond[$name])){
 				unset($this->selectingSecond[$name]);
-
-				$this->secondPosition[$name] = $pos;
-
-				$player->sendMessage("Pos2 set");
-
+				$this->secondPosition[$name]=new Vector3($x,$y,$z);
 				$event->cancel();
 				return;
 			}
-
-			if(!$this->canEdit($player,new Position($x,$y,$z,$player->getWorld()))){
-				$event->cancel();
-			}
 		}
 	}
 
-	public function onBreak(BlockBreakEvent $event) : void{
+	public function onBreak(BlockBreakEvent $event) : void{}
 
-		if(!$this->canEdit($event->getPlayer(),$event->getBlock()->getPosition())){
-			$event->cancel();
-		}
-	}
+	public function onBucket(PlayerBucketEvent $event) : void{}
 
-	public function onBucket(PlayerBucketEvent $event) : void{
-
-		if(!$this->canEdit($event->getPlayer(),$event->getBlockClicked()->getPosition())){
-			$event->cancel();
-		}
-	}
-
-	public function onDamage(EntityDamageEvent $event) : void{
-
-		$entity = $event->getEntity();
-
-		if(!$entity instanceof Player){
-			return;
-		}
-
-		foreach($this->areas as $area){
-
-			if($area->contains($entity->getPosition(),$entity->getWorld()->getFolderName())){
-
-				if($area->getFlag("god")){
-					$event->cancel();
-				}
-			}
-		}
-	}
+	public function onDamage(EntityDamageEvent $event) : void{}
 }
